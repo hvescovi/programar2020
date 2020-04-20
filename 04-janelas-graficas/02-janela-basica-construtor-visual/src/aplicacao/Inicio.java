1ea2e2b4d197f45f35ecc06964e8b75fee9857d8
Hyl <hvescovi@gmail.com>
2020-04-20 20:33:21 -0300

    ajustes na janela básica eclipse/wb

---
+++ 04-janelas-graficas/02-janela-basica-construtor-visual/src/aplicacao/Inicio.java
@@ -0,0 +1,11 @@
+package aplicacao;
+
+import visao.Basica;
+
+public class Inicio {
+
+	public static void main(String args[]) {
+		Basica jan = new Basica();
+		jan.setVisible(true);
+	}
+}
--- 04-janelas-graficas/02-janela-basica-construtor-visual/src/visao/Basica.java
+++ 04-janelas-graficas/02-janela-basica-construtor-visual/src/visao/Basica.java
@@ -1,42 +1,50 @@
 package visao;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Basica extends JFrame {
 
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Basica frame = new Basica();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
 	public Basica() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		// configurações básicas da janela
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // não é preciso "this."
+		setBounds(0, 0, 225, 140); // tamanho da janela
+		setLocationRelativeTo(null); // centralizar a janela
+		setTitle("Básica Java WM"); 
+						
+		JPanel painel = new JPanel(); // painel de componentes
+		painel.setLayout(null); // layout absoluto
+		setContentPane(painel); // adicionar painel à janela
+		
+		//rótulos
+		JLabel lblMensagem = new JLabel("");
+		// coordenadas e dimensões do componente: x, y, largura, altura 
+		lblMensagem.setBounds(29, 60, 180, 15); 
+		painel.add(lblMensagem);
+		
+		JLabel lblTitulo = new JLabel("Digite o seu nome:");
+		lblTitulo.setBounds(33, 12, 133, 15);
+		painel.add(lblTitulo);
+		
+		// caixa de entrada
+		JTextField txtNome = new JTextField();
+		txtNome.setBounds(29, 35, 114, 19);
+		painel.add(txtNome);
+				
+		// botão
+		JButton btnOk = new JButton("Ok");
+		btnOk.setBounds(153, 31, 53, 26);
+		// ação do botão
+		btnOk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				lblMensagem.setText("O seu nome é: "+txtNome.getText());
+			}
+		});		
+		painel.add(btnOk);		
 	}
-
 }
