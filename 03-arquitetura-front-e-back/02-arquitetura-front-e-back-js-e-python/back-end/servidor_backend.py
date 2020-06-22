e188b0eb2a474ff78fde7ebc4638458f09e1a900
Hylson <hvescovi@gmail.com>
2020-06-22 12:16:04 -0300

    front-end javascript

---
+++ 03-arquitetura-front-e-back/02-arquitetura-front-e-back-js-e-python/back-end/servidor_backend.py
@@ -0,0 +1,21 @@
+from config import *
+from modelo import Pessoa
+
+@app.route("/")
+def inicio():
+    return 'Sistema de cadastro de pessoas. '+\
+        '<a href="/listar_pessoas">Operação listar</a>'
+
+@app.route("/listar_pessoas")
+def listar_pessoas():
+    # obter as pessoas do cadastro
+    pessoas = db.session.query(Pessoa).all()
+    # aplicar o método json que a classe Pessoa possui a cada elemento da lista
+    pessoas_em_json = [ x.json() for x in pessoas ]
+    # converter a lista do python para json
+    resposta = jsonify(pessoas_em_json)
+    # PERMITIR resposta para outras pedidos oriundos de outras tecnologias
+    resposta.headers.add("Access-Control-Allow-Origin", "*")
+    return resposta # retornar...
+
+app.run(debug=True)
