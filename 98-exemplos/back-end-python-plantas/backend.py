131445fae520e5b53172d6fd1ea10e42bb0c3745
Hylson <hvescovi@gmail.com>
2020-08-03 14:59:47 -0300

    código demonstração show com erro no finalzinho

---
+++ 98-exemplos/back-end-python-plantas/backend.py
@@ -0,0 +1,24 @@
+from config import *
+from modelo import Planta
+
+@app.route("/")
+def padrao():
+    return "bem vindo ao backend"
+
+@app.route("/listar_plantas")
+def listar_plantas():
+    plantas = db.session.query(Planta).all()
+    retorno = []    
+    for p in plantas:
+        retorno.append(p.json())
+    return jsonify(retorno)
+
+@app.route("/incluir_planta", methods=['post'])
+def incluir_planta():
+    dados = request.get_json()
+    nova_planta = Planta(**dados) #Planta(nome = dados['nome'], nome_cientifico = dados['nome_cientifico']...
+    db.session.add(nova_planta)
+    db.session.commit()
+    return {"resultado":'ok'}
+
+app.run(debug=True)
