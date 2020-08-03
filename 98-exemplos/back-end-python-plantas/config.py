ff33dd71722412dda587a9d6d3c89e8ae306484f
Hylson <hvescovi@gmail.com>
2020-08-03 15:51:23 -0300

    erro ajustado, coisa maluca

--- 98-exemplos/back-end-python-plantas/config.py
+++ 98-exemplos/back-end-python-plantas/config.py
@@ -7,27 +7,3 @@ arquivobd = os.path.join(caminho, "plantas.db")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+arquivobd 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # remover warnings 
 db = SQLAlchemy(app)
-
-from modelo import Planta
-
-@app.route("/")
-def padrao():
-    return "bem vindo ao backend"
-
-@app.route("/listar_plantas")
-def listar_plantas():
-    plantas = db.session.query(Planta).all()
-    retorno = []    
-    for p in plantas:
-        retorno.append(p.json())
-    return jsonify(retorno)
-
-@app.route("/incluir_planta", methods=['post'])
-def incluir_planta():
-    dados = request.get_json()
-    nova_planta = Planta(**dados) #Planta(nome = dados['nome'], nome_cientifico = dados['nome_cientifico']...
-    db.session.add(nova_planta)
-    db.session.commit()
-    return {"resultado":'ok'}
-
-app.run(debug=True)
