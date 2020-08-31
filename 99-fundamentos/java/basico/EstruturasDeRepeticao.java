422041ee7c29d91526629cbf00162724cafd4d90
Hylson Netto <hvescovi@gmail.com>
2020-08-31 09:46:14 -0300

    estruturas de repetição for e do-while

--- 99-fundamentos/java/basico/EstruturasDeRepeticao.java
+++ 99-fundamentos/java/basico/EstruturasDeRepeticao.java
@@ -1,5 +1,7 @@
 package basico;
 
+import java.util.Random;
+
 public class EstruturasDeRepeticao {
 
 	public static void main(String args[]) {
@@ -15,13 +17,48 @@ public class EstruturasDeRepeticao {
 		int sentinela = 0; // controlador da repetição
 		contador = 1;
 		while (sentinela == 0) {
-			System.out.printf("%d ", contador);
-			contador++;
-			if (contador == 11) { // condição de fim da repetição
+			if (contador % 4 == 0 && contador % 3 == 0 && contador > 20) {
 				sentinela = 1; //sinalizar encerramento de repetição
+				System.out.printf("Valor que atende a condição: %d", contador);
+			} else {
+				contador++;
 			}
 		}
 		
+		System.out.print("\nRepetição com for: ");
+		for (int i = 1; i <= 10; i++) {
+			System.out.printf("%d ", i);
+		}
+		System.out.print("\nRepetição com for (passo negativo): ");
+		for (int i = 10; i > 0; i-=2) {
+			System.out.printf("%d ", i);
+		}
 		
+		System.out.print("\nRepetição do while");
+		int n; // variável que vai receber números aleatórios sorteados
+		Random r  = new Random(); // gerador de números aleatórios
+		do {
+			n = r.nextInt(5); // sorteia um número inteiro entre 0 e 5
+			System.out.printf("\nNúmero escolhido: %d", n);
+		} while (n != 1); // enquanto n não for igual a 1, repete
+		
+		System.out.println("Desenho de um triângulo com for");
+		for (int i = 1; i <= 10; i++) { // desenhar um triângulo
+			System.out.println(("*").repeat(i));
+		}
+		/*		o resultado do for acima é o desenho deste triângulo (linhas 51 a 60)
+		
+		*
+		**
+		***
+		****
+		*****
+		******
+		*******
+		********
+		*********
+		**********
+
+		 */
 	}	
 }
