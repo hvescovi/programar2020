c0a22c1a3685285a9503fc2907d8727fa0c9f36b
Hylson <hvescovi@gmail.com>
2020-10-10 22:05:00 -0300

    ajustes no n-x-n classes python

--- 03-arquitetura-front-e-back/06-arquitetura-front-e-back-n-para-n-python/back-end/modelo.py
+++ 03-arquitetura-front-e-back/06-arquitetura-front-e-back-n-para-n-python/back-end/modelo.py
@@ -25,10 +25,8 @@ class Exame(db.Model):
     nome = db.Column(db.String(254)) # nome do exame
     unidade = db.Column(db.String(254)) # unidade de medida
     vr = db.Column(db.String(254)) # valores de referência
-    
     def __str__(self):
         return f"{self.nome} [{self.id}], unidade={self.unidade} ({self.vr})"  
-
     def json(self):
         return {
             "id":self.id,
