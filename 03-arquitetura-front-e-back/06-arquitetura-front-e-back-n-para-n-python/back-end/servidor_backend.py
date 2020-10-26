0e7a42facfc1b958dbc643a94c42e5d2bb240c70
Hylson <hvescovi@gmail.com>
2020-10-26 04:59:37 -0300

    generalizando rota de listar dados

--- 03-arquitetura-front-e-back/06-arquitetura-front-e-back-n-para-n-python/back-end/servidor_backend.py
+++ 03-arquitetura-front-e-back/06-arquitetura-front-e-back-n-para-n-python/back-end/servidor_backend.py
@@ -1,5 +1,5 @@
 from config import *
-from modelo import Pessoa, ExameRealizado, Respirador
+from modelo import Pessoa, ExameRealizado, Respirador, Exame
 
 @app.route("/")
 def inicio():
@@ -99,43 +99,49 @@ $ curl localhost:5000/listar_exames_realizados
 ]
 '''
 
-@app.route("/listar_respiradores")
-# outra função similar à listar_pessoa...
-def listar_respiradores():
-    # obter exames realizados
-    respiradores = db.session.query(Respirador).all()
+@app.route("/listar/<string:classe>")
+# mais uma função similar à listar_pessoa :-/ vamos tentar generalizar :-)
+def listar(classe):
+    # obter os dados da classe informada
+    dados = None
+    if classe == "ExameRealizado":
+      dados = db.session.query(ExameRealizado).all()
+    elif classe == "Pessoa":
+      dados = db.session.query(Pessoa).all()
+    elif classe == "Respirador":
+      dados = db.session.query(Respirador).all()
+    elif classe == "Exame":
+      dados = db.session.query(Exame).all()
     # converter dados para json
-    lista_jsons = [ x.json() for x in respiradores ]
+    lista_jsons = [ x.json() for x in dados ]
     # converter a lista do python para json
     resposta = jsonify(lista_jsons)
     # PERMITIR resposta para outras pedidos oriundos de outras tecnologias
     resposta.headers.add("Access-Control-Allow-Origin", "*")
     return resposta
-'''
-$ curl localhost:5000/listar_respiradores
-[
-  {
-    "codigo": "001A", 
-    "data_aquisicao": "24/03/2020", 
-    "data_emprestimo": "", 
-    "id": 1, 
-    "pessoa": "", 
-    "pessoa_id": ""
-  }, 
-  {
-    "codigo": "002B", 
-    "data_aquisicao": "01/02/2020", 
-    "data_emprestimo": "04/02/2020", 
-    "id": 2, 
-    "pessoa": {
-      "email": "josilva@gmail.com", 
-      "id": 1, 
-      "nome": "Jo\u00e3o da Silva", 
-      "telefone": "47 99012 3232"
-    }, 
-    "pessoa_id": 1
-  }
-]
+    '''
+    $ curl localhost:5000/listar/ExameRealizado
+    [
+      {
+        "data": "02/02/2020", 
+        "exame": {
+          "id": 1, 
+          "nome": "B12", 
+          "unidade": "pg/mL", 
+          "vr": "239 a 931"
+        }, 
+        "exame_id": 1, 
+        "id": 1, 
+        "pessoa": {
+          "email": "josilva@gmail.com", 
+          "id": 1, 
+          "nome": "Jo\u00e3o da Silva", 
+          "telefone": "47 99012 3232"
+        }, 
+        "pessoa_id": 1, 
+        "resultado": "219,0 pg/mL"
+      }
+    ]
 '''
 
 app.run(debug=True)
